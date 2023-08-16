6f8e031a9e1b16ebd15be4be6a109204dcd439d5
BuildTools <unconfigured@null.spigotmc.org>
2023-08-16 04:50:21 -0300

    Add next-reveal

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,50 +2,51 @@
 import styles from "@/styles/Contact.module.css";
 import { useEffect } from "react";
 import { useStatesContext } from "@/components/StatesContext";
-import { scrollReveal } from "@/utils/ScrollReveal";
+import { RevealWrapper } from  "next-reveal";
 
 const Contact = () => {
   const { setActiveLink } = useStatesContext();
  
-  useEffect(() => {
-    setActiveLink("/contact");
-    scrollReveal();
-  }, []);
+  useEffect(() => setActiveLink("/contact"), []);
 
   return (
     <main className={styles.contact}>
-      <div className={`${styles.mail} left-anim`}>
-        <img
-          className={styles.option}
-          src="/assets/Logo-Mail.png"
-          alt="Mail"
-        />
-        <div className={styles.description}>
-          <span>Contactar por Mail</span>
-          <div className={styles.text}>
-            <p>Adicionalmente, también podés enviarnos un email a nuestro correo eléctronico.</p>
-            <div className={styles.info}>
-              <p>solplast-mr@hotmail.com</p>
+      <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+        <div className={styles.mail}>
+          <img
+            className={styles.option}
+            src="/assets/Logo-Mail.png"
+            alt="Mail"
+          />
+          <div className={styles.description}>
+            <span>Contactar por Mail</span>
+            <div className={styles.text}>
+              <p>Adicionalmente, también podés enviarnos un email a nuestro correo eléctronico.</p>
+              <div className={styles.info}>
+                <p>solplast-mr@hotmail.com</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={`${styles.whatsapp} right-anim`}>
-        <div className={styles.description}>
-          <span>Contactar por WhatsApp</span>
-          <div className={styles.text}>
-            <p>Podés enviarnos tu consulta a través de nuestro número por WhatsApp.</p>
-            <div className={styles.info}>
-              <p>+54 9 11 6188-8416</p>
+      </RevealWrapper>
+      <RevealWrapper origin="right" distance="40px" duration={1500} delay={200} >
+        <div className={styles.whatsapp}>
+          <div className={styles.description}>
+            <span>Contactar por WhatsApp</span>
+            <div className={styles.text}>
+              <p>Podés enviarnos tu consulta a través de nuestro número por WhatsApp.</p>
+              <div className={styles.info}>
+                <p>+54 9 11 6188-8416</p>
+              </div>
             </div>
           </div>
+          <img
+            className={styles.option}
+            src="/assets/Logo-WhatsApp.png"
+            alt="WhatsApp"
+          />
         </div>
-        <img
-          className={styles.option}
-          src="/assets/Logo-WhatsApp.png"
-          alt="WhatsApp"
-        />
-      </div>
+      </RevealWrapper>
     </main>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -3,99 +3,132 @@ import styles from "@/styles/Home.module.css";
 import { useEffect } from "react";
 import Button from "@/components/Button";
 import { useStatesContext } from "@/components/StatesContext";
-import { scrollReveal } from "@/utils/ScrollReveal";
+import { RevealWrapper } from "next-reveal";
 
 const Home = () => {
   const { setActiveLink } = useStatesContext();
 
-  useEffect(() => {
-    setActiveLink("/");
-    scrollReveal();
-  }, []);
+  useEffect(() => setActiveLink("/"), []);
 
   return (
     <main className={styles.main}>    
       <section className={styles.home}>
         <div className={styles.logo}>
-          <img 
-            className={`${styles["section-logo"]} top-anim`}
-            src="/assets/Logo-SOLPLAST.png"
-            alt="SOLPLAST®"
-          />
-          <p className="bottom-anim">Fábrica de todo tipo de repuestos y accesorios para piletas de lona</p>  
+          <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
+            <img 
+              className={styles["section-logo"]}
+              src="/assets/Logo-SOLPLAST.png"
+              alt="SOLPLAST®"
+            />
+          </RevealWrapper>
+          <RevealWrapper origin="bottom" distance="40px" duration={1500} delay={200} >
+            <p>Fábrica de todo tipo de repuestos y accesorios para piletas de lona</p>
+          </RevealWrapper>
         </div>
         <div className={styles.history}>
-          <div className="top-anim">
+          <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
             <span>Un poco de historia...</span>
-          </div>
-          <p className="left-anim">SOLPLAST® nació hace más de 40 años como un emprendimiento familiar. Con apenas un pequeño espacio, una máquina de coser y un inmenso sueño por delante, comenzamos nuestro viaje.</p>
-          <p className="right-anim">El camino no fue fácil al principio; con pocos clientes, enfrentamos desafíos, pero con perseverancia, calidad y atención al detalle, nuestra marca fue creciendo.</p>
-          <div className={`${styles.buttons} bottom-anim`}>
-            <Button href="#sp" className={styles.button} label="Leer más"/>
-            <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
-          </div>
+          </RevealWrapper>
+          <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+            <p>SOLPLAST® nació hace más de 40 años como un emprendimiento familiar. Con apenas un pequeño espacio, una máquina de coser y un inmenso sueño por delante, comenzamos nuestro viaje.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="right" distance="40px" duration={1500} delay={200} >
+            <p>El camino no fue fácil al principio; con pocos clientes, enfrentamos desafíos, pero con perseverancia, calidad y atención al detalle, nuestra marca fue creciendo.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="bottom" distance="40px" duration={1500} delay={200} >
+            <div className={styles.buttons}>
+              <Button href="#sp" className={styles.button} label="Leer más"/>
+              <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
+            </div>
+          </RevealWrapper>
         </div>
       </section>
       <section id="sp" className={styles.home}>
         <div className={styles.history}>
-          <div className="top-anim">
+          <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
             <span>Nacimiento de SP®</span>
-          </div>
-          <p className="left-anim">Con el tiempo, nuestras habilidades y diversidad también se desarrollaron. Fue entonces cuando decidimos ampliar nuestra oferta con una marca derivada de SOLPLAST®: SP®.</p>
-          <p className="right-anim">A través de SP®, nos adentramos en el mundo de los adhesivos para reparación de piletas de lona y zapatillas.</p>
-          <div className={`${styles.buttons} bottom-anim`}>
-            <Button href="#pets" className={styles.button} label="Leer más"/>
-            <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
-          </div>
+          </RevealWrapper>
+          <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+            <p>Con el tiempo, nuestras habilidades y diversidad también se desarrollaron. Fue entonces cuando decidimos ampliar nuestra oferta con una marca derivada de SOLPLAST®: SP®.</p>
+          </RevealWrapper> 
+          <RevealWrapper origin="right" distance="40px" duration={1500} delay={200} >
+            <p>A través de SP®, nos adentramos en el mundo de los adhesivos para reparación de piletas de lona y zapatillas.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="bottom" distance="40px" duration={1500} delay={200} >
+            <div className={styles.buttons}>
+              <Button href="#pets" className={styles.button} label="Leer más"/>
+              <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
+            </div>
+          </RevealWrapper>
         </div>
-        <div className={`${styles.logo} top-anim`}>
-          <img
-            className={styles["section-logo"]}
-            src="/assets/Logo-SP.png"
-            alt="SP®"
-          />
-        </div>
+        <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
+          <div className={styles.logo}>
+            <img
+              className={styles["section-logo"]}
+              src="/assets/Logo-SP.png"
+              alt="SP®"
+            />
+          </div>
+        </RevealWrapper>
       </section>
       <section id="pets" className={styles.home}>
-        <div className={`${styles.logo} top-anim`}>
-          <img
-            className={styles["section-logo"]}
-            src="/assets/Logo-Mascotas.png"
-            alt="Mascotas"
-          />
-        </div>
+        <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
+          <div className={styles.logo}>
+            <img
+              className={styles["section-logo"]}
+              src="/assets/Logo-Mascotas.png"
+              alt="Mascotas"
+            />
+          </div>
+        </RevealWrapper>
         <div className={styles.history}>
-          <div className="top-anim">
+          <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
             <span>Productos para mascotas</span>
-          </div>
-          <p className="left-anim">Recientemente, llegaron a nuestra familia de productos las colchonetas para mascotas, los bolsitos y los platos de comida para perritos y gatitos.</p>
-          <p className="right-anim">Porque para nosotros, ellos son más que simples compañeros, son parte esencial de nuestros hogares y merecen lo mejor.</p>
-          <div className={`${styles.buttons} bottom-anim`}>
-            <Button href="#thanks" className={styles.button} label="Leer más" />
-            <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
-          </div>
+          </RevealWrapper>
+          <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+            <p>Recientemente, llegaron a nuestra familia de productos las colchonetas para mascotas, los bolsitos y los platos de comida para perritos y gatitos.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="right" distance="40px" duration={1500} delay={200} >
+            <p>Porque para nosotros, ellos son más que simples compañeros, son parte esencial de nuestros hogares y merecen lo mejor.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="bottom" distance="40px" duration={1500} delay={200} >
+            <div className={styles.buttons}>
+              <Button href="#thanks" className={styles.button} label="Leer más" />
+              <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
+            </div>
+          </RevealWrapper>
         </div>
       </section>
       <section id="thanks" className={styles.home}>
         <div className={styles.history}>
-          <div className="top-anim">
+          <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
             <span>Actualidad</span>
-          </div>
-          <p className="left-anim">Hoy en día, SOLPLAST® sigue siendo una empresa familiar, arraigada en la tradición de la calidad, la innovación y el servicio al cliente.</p>
-          <p className="right-anim">Cada día, nos esforzamos por superar nuestras propias expectativas y crear productos que enriquezcan la vida de las personas y sus adorables mascotas.</p>
-          <p className="left-anim">Gracias por ser parte de nuestro viaje, gracias por ser parte de SOLPLAST®.</p>
-          <div className={`${styles.buttons} bottom-anim`}>
-            <Button href="#" className={styles.button} label="Volver"/>
-            <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
-          </div>
+          </RevealWrapper>
+          <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+            <p>Hoy en día, SOLPLAST® sigue siendo una empresa familiar, arraigada en la tradición de la calidad, la innovación y el servicio al cliente.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="right" distance="40px" duration={1500} delay={200} >
+            <p>Cada día, nos esforzamos por superar nuestras propias expectativas y crear productos que enriquezcan la vida de las personas y sus adorables mascotas.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="left" distance="40px" duration={1500} delay={200} >
+            <p>Gracias por ser parte de nuestro viaje, gracias por ser parte de SOLPLAST®.</p>
+          </RevealWrapper>
+          <RevealWrapper origin="bottom" distance="40px" duration={1500} delay={200} >
+            <div className={`${styles.buttons} bottom-anim`}>
+              <Button href="#" className={styles.button} label="Volver"/>
+              <Button href="/products" className={styles["button-secondary"]} label="Ver productos" />
+            </div>
+          </RevealWrapper>
         </div>
-        <div className={`${styles.logo} top-anim`}>
-          <img
-            className={styles["section-logo"]}
-            src="/assets/Logo-Apreton.png"
-            alt="Actualidad"
-          />
-        </div>
+        <RevealWrapper origin="top" distance="40px" duration={1500} delay={200} >
+          <div className={styles.logo}>
+            <img
+              className={styles["section-logo"]}
+              src="/assets/Logo-Apreton.png"
+              alt="Actualidad"
+            />
+          </div>
+        </RevealWrapper>
       </section>
     </main>
   );
